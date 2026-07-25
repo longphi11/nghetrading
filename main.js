a93a24eeae3e37ae539bed3a75b4a16b033a7a23
longphibtc <longphi11@gmail.com>
2026-07-25 19:16:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -217,7 +217,7 @@ const DISQUS_SHORTNAME = "nghetrading";
   const isArticleListPage = location.pathname.endsWith('bai-viet.html');
   const isSystemPage = location.pathname.endsWith('he-thong.html');
   const isJournalPage = location.pathname.endsWith('nhat-ky.html');
-  if (!isPostPage && !isArticleListPage && !isSystemPage) return;
+  if (!isPostPage && !isArticleListPage && !isSystemPage && !isJournalPage) return;
 
   if (sessionStorage.getItem('cafeCtaDismissed') === '1') return;
 
