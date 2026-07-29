09cce1527f675fcdf6a33ca8ddc241fd86916c02
longphibtc <longphi11@gmail.com>
2026-07-29 19:34:51 +0700

    style: Reduce mobile Telegram float button size by 25% and rename text to Tâm sự trading

--- main.js
+++ main.js
@@ -330,7 +330,7 @@ const DISQUS_SHORTNAME = "nghetrading";
         <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
       </svg>
     </a>
-    <div class="telegram-float-text">Hỗ trợ</div>
+    <div class="telegram-float-text">Tâm sự trading</div>
   `;
   document.body.appendChild(widget);
 
